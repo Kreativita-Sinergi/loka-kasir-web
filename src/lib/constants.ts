b78aa3278d064be3586fd6e0a0679b0cf3a327b8
odhi ahmad <odhiahmad15@gmail.com>
2026-06-14 16:25:04 +0700

    fix(constants): update APK download URL
    replace the outdated Google Drive APK download link with the new valid one

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@
 // Saat ini di-host di Google Drive — buka halaman Drive lalu pengguna tap unduh
 // (lebih andal untuk file APK besar dibanding tautan unduh langsung).
 export const APK_DOWNLOAD_URL =
-  'https://drive.google.com/file/d/1who638FSNDPcAaUp5JJ9J6G16g1k35H_/view?usp=drive_link'
+  'https://drive.google.com/file/d/1RC47PWGOczKE5jbVQEzX_-m98xCWZx7k/view?usp=drive_link'
